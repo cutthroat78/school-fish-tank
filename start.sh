722d657d2b08d7db744fbf05dfc9c462ddc72265
cutthroat78 <irisharmycat2@gmail.com>
2022-09-28 10:55:13 +0100

    Made camera run

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #/bin/sh
 
 export LD_LIBRARY_PATH=.
-cd ~/mjpg_streamer/mjpg-streamer-experimental/
-sh ./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -hf"
+mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -hf"
