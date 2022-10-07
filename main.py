6e55831a6ec8b146c49be5691263a71419892671
cutthroat78 <irisharmycat2@gmail.com>
2022-10-07 20:46:30 +0100

    setup for flask to show camera on website

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template
 from plant_monitor import PlantMonitor
+from camera import Camera
+
 pm = PlantMonitor()
 
 app = Flask(__name__)
@@ -15,6 +17,17 @@ def index():
             'humidity' : str(pm.get_humidity())
             }
     return render_template('index.html', **templateData)
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
