fe4707fa8f7cbb1c155e53f3b5c0ad7bfd9071c7
cutthroat78 <irisharmycat2@gmail.com>
2022-10-07 19:15:17 +0100

    made csv.py make file if file doesn't exist

--- csv.py
+++ csv.py
@@ -8,11 +8,13 @@ pm = PlantMonitor()
 
 filename = "~/data/" + sys.argv[1]
 
+if os.path.exists(file_path):
+else:
+    with open(filename, 'w') as fp:
+        f.write("time,water level(%),temp(°C),humidity(%)\n")
+
 f = open(filename, "w")
 
 t = time.strftime("%H:%M:%S", time.localtime())
-
-if os.stat(filename).st_size == 0:
-    f.write("time,water level(%),temp(°C),humidity(%)\n")
 
 f.write(t + "," + str(pm.get_wetness()) + "," + str(pm.get_temp()) + "," + str(pm.get_humidity()))
