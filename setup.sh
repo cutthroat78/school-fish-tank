2df10f2ce065a83568b6327e9051be1a15e32199
cutthroat78 <irisharmycat2@gmail.com>
2022-10-07 16:49:44 +0100

    Sorted out setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,8 @@
 
 sudo apt update
 sudo apt upgrade -y
+sudo apt install git python3-flask cmake python-pil libjpeg-dev -y
 git clone https://github.com/jacksonliam/mjpg-streamer.git
-sudo apt install cmake python-pil libjpeg-dev -y
 cd mjpg-streamer/mjpg-streamer-experimental/
 make CMAKE_BUILD_TYPE=Debug
 sudo make install
-sudo apt install ngrok python3-flask node
-
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
