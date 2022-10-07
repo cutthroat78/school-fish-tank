297f6736a7180ddad41710c1bd5c7c324a8450fc
cutthroat78 <irisharmycat2@gmail.com>
2022-10-07 21:30:26 +0100

    1

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #/bin/sh
 
-export LD_LIBRARY_PATH=.
-mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -hf" &
-python3 ~/School-Fish-Tank/main.py
+sudo python3 ~/School-Fish-Tank/main.py
